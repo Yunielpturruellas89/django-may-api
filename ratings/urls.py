cc6afd05ed862ab35f82e5aad40bc1583382eef8
@yunielpturruellas <yunielpturruellas@gmail.com>
2024-11-26 11:52:55 -0500

    fix url ratings

--- ratings/urls.py
+++ ratings/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from .views import RatingViewSet
 
 router = routers.DefaultRouter()
-router.register(r'raings', RatingViewSet) #  'hymns' es el prefijo de la URL
+router.register(r'ratings', RatingViewSet) #  'hymns' es el prefijo de la URL
 urlpatterns = [
     path('', include(router.urls)),
 ]
