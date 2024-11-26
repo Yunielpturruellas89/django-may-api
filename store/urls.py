89d531d3a88f72e10e6e13ce1d0aeb6cc2f52211
@yunielpturruellas <yunielpturruellas@gmail.com>
2024-11-26 11:36:21 -0500

    fix: urls

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,6 @@
-from django.urls import path,include
+from django.urls import path
 from rest_framework import routers
 from .views import ProductViewSet
-from ratings.urls import urlpatterns as ratings_urls
 router = routers.DefaultRouter()
-router.register(r'products', ProductViewSet) #  'hymns' es el prefijo de la URL
-urlpatterns = [
-    path('', include(router.urls)),
-    path('', include(ratings_urls)), # Include ratings URLs here
-]
+router.register(r'products', ProductViewSet) 
+urlpatterns = router.urls
